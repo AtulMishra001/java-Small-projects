6e01ce5e198ab3530e5977e5c44e89f95c52be86
AtulMishra001 <atulm7251@gmail.com>
2024-01-09 21:05:52 +0530

    palindromeNumber file is added

---
+++ palindromeNumber.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class palindromeNumber {
+    public static void checkPalindrome(int num) {
+        int reverseNum=0;
+        int myNum = num;
+        while (num >0) {
+            int lastDigit = num % 10;
+            reverseNum = reverseNum *10 +(lastDigit);
+            num = num / 10;
+        }
+
+        if(myNum == reverseNum){
+            System.out.println(myNum+" is a Palindrome number");
+        }
+        else{
+            System.out.println(myNum+" is not a Palindrome number\nReverse of "+myNum+" is "+reverseNum+" and this is not identical to our input "+myNum);
+        }
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to check if number is palindrome");
+        int num = sc.nextInt();
+        checkPalindrome(num);
+        sc.close();
+    }
+}
